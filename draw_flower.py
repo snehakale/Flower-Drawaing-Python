f9878bc11472f9312d52e971fb33ef7aa26f7058
Sneha Kale <kalesneha88@gmail.com>
2018-05-02 15:02:30 -0400

    style: Format code and add comments
    Format code and add comments in draw_flower python program.

--- draw_flower.py
+++ draw_flower.py
@@ -1,22 +1,27 @@
+# Import module turtle to draw shapes 
 import turtle
 
+# Function to draw a rectangle 
 def draw_rectangle(some_turtle):
     for x in range(1,5):
         some_turtle.forward(50)
-        if (x%2==1):
+        if (x%2 == 1):
             some_turtle.right(60)
         else:
             some_turtle.right(120)
 
 
+# Function to draw a flower
 def draw_flower():
+    # Creating a screen for a turtle to draw
     window = turtle.Screen()
     window.bgcolor("black")
-    
+
+    # Creating a turtle object
     flower = turtle.Turtle()
     flower.shape("circle")
     flower.color("red")
-    flower.speed(1)
+    flower.speed(10)
 
     for i in range(1,37):
         draw_rectangle(flower)
@@ -25,8 +30,9 @@ def draw_flower():
     flower.color("green")
     flower.right(90)
     flower.forward(200)
-
-    window.exitonclick()
     
+    window.exitonclick()
+
+# Calling a function to draw a flower
 draw_flower()   
     
